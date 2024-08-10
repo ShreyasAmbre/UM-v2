1858b9edf4314679cf71018d0c927c2247d945ec
ShreyasAmbre <shreyasambre00@gmail.com>
2024-08-10 21:38:01 +0530

    feat: ng zorro package integrated in antd lib module

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -4,9 +4,14 @@ import { RouterModule } from '@angular/router';
 import { authRoutes } from './lib.routes';
 import { LoginFormComponent } from './components/login-form.component';
 import { LoginComponent } from './components/containers/login.component';
+import { AntdUiModule } from '@um-workspace/antd-ui';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(authRoutes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(authRoutes),
+    AntdUiModule
+  ],
   declarations: [LoginComponent, LoginFormComponent],
   exports: [LoginComponent, LoginFormComponent],
 })
--- libs/shared/antd/src/lib/antd-ui.module.ts
+++ libs/shared/antd/src/lib/antd-ui.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NzButtonModule,
+    NzIconModule
+  ],
+  exports: [
+    NzButtonModule,
+    NzIconModule
+  ]
 })
 export class AntdUiModule {}
