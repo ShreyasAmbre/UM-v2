b3ad0c13efd6c361ccfbb6e6efa5fbecf549ac0a
ShreyasAmbre <shreyasambre00@gmail.com>
2024-08-10 15:21:57 +0530

    fet : login container created

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { authRoutes } from './lib.routes';
+import { LoginComponent } from './components/containers/login.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(authRoutes)],
+  declarations: [LoginComponent],
+  exports: [LoginComponent],
 })
 export class AuthModule {}
---
+++ libs/auth/src/lib/components/containers/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.less',
+})
+export class LoginComponent {}
